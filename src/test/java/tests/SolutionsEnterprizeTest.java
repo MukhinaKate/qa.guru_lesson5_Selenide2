4b3c4f22417d1dfdcf56229ecc2e7aa74bb177fd
MukhinaKate <ekaterina.dmitrievna96@gmail.com>
2025-02-12 22:39:20 +0300

    add SolutionsEnterprizeTest

---
+++ src/test/java/tests/SolutionsEnterprizeTest.java
@@ -0,0 +1,28 @@
+package tests;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class SolutionsEnterprizeTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = false; //чтобы браузер не закрывался
+
+    }
+    @Test
+    void fillFormTest() {
+        open("https://github.com");
+        $(".HeaderMenu-nav").$(byText("Solutions")).hover();
+        $(".HeaderMenu-nav").$(byText("Enterprises")).click();
+        $("#hero-section-brand-heading").shouldHave(text("The AI-powered developer platform"));
+
+
+    }
+}
